259a472b41187a6f49bd9d8de3e1d4953d2bafcf
Airat Vagapov <Airat-vagapov>
2023-10-03 23:59:57 +0500

    update

--- src/modules/RightCard.vue
+++ src/modules/RightCard.vue
@@ -1,16 +1,11 @@
 <template>
     <BaseBigCard class="right__side" :width="70">
         <TodayHighlights></TodayHighlights>
-<<<<<<< HEAD
         <ForecastWeather></ForecastWeather>
-=======
-        
->>>>>>> 177969e (update)
     </BaseBigCard>
 </template>
 
 <script>
-<<<<<<< HEAD
 // import { mapGetters } from 'vuex';
 import TodayHighlights from '@/components/TodayHighlights.vue';
 import ForecastWeather from '@/components/ForecastWeather.vue'
@@ -43,38 +38,6 @@ export default {
     },
     mounted() {
         // console.log(this.forecastWeatherData)
-=======
-import { mapGetters } from 'vuex';
-import TodayHighlights from '@/components/TodayHighlights.vue';
-export default {
-    components: {
-        TodayHighlights
-    },
-    computed: {
-        ...mapGetters(['weatherData', 'forecastWeatherData']),
-        astroData() {
-            let result;
-
-            if (this.weatherData.is_day === 1) {
-                result = this.forecastWeatherData[0].astro.sunset
-            } else {
-                result = this.forecastWeatherData[0].astro.sunrise
-            }
-            return result
-        },
-        astroDataName() {
-            let astroName
-            if (this.weatherData.is_day === 1) {
-                astroName = 'Sunset'
-            } else {
-                astroName = 'Sunrise'
-            }
-            return astroName
-        }
-    },
-    mounted() {
-        console.log(this.forecastWeatherData)
->>>>>>> 177969e (update)
     }
 }
 </script>
