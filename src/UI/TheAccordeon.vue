e4760fb04d96392174257d3f84e8c0c6ec64568c
Airat Vagapov <Airat-vagapov>
2023-11-12 12:03:10 +0300

    create detail accordeon

---
+++ src/UI/TheAccordeon.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="accordeon">
+        <div class="accordeon__titleBlock" @click="openContent">
+            <p class="accordeon__title">{{ this.title }}</p>
+            <BaseIcon class="location__icon" :icon="'expand_less'"></BaseIcon>
+        </div>
+        <Transition name="slide">
+            <div v-if="this.isOpen" class="accordeon__body">
+                <slot name="content"></slot>
+            </div>
+        </Transition>
+    </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+    data() {
+        return {
+            isOpen: false
+        }
+    },
+    props: ['title'],
+    computed: {
+        ...mapGetters(['deviceType'])
+    },
+    methods: {
+        openContent() {
+            this.isOpen === true ? this.isOpen = false : this.isOpen = true
+            
+        }
+    },
+    mounted() {
+        // const
+    }
+}
+</script>
+
+<style scoped lang="sass">
+.accordeon__titleBlock
+    display: flex
+    align-items: center
+    justify-content: space-between
+    .accordeon__title 
+        font-weight: 400
+</style>
--- src/components/TodayHighlights.vue
+++ src/components/TodayHighlights.vue
@@ -1,6 +1,6 @@
 <template>
     <BaseText :size="24" :marginBot="24" v-if="showTitle">Today's highlights</BaseText>
-    <BaseCard class="todayBlock" :type="'grid'" :column="3" :padding="'none'">
+    <BaseCard class="todayBlock" :type="'grid'" :column="3" :padding="'none'" v-if="this.deviceType === 'desktop'">
         <BaseCard class="todayCard">
             <span class="todayCard__value"> {{ forecastWeatherData[0].day.daily_chance_of_rain + " " +
                 "%" }}</span>
@@ -23,19 +23,53 @@
             <span class="todayCard__value">{{ forecastWeatherData[0].day.uv }}</span>
             <p class="todayCard__text">UV</p>
         </BaseCard>
-        <BaseCard class="todayCard" v-if="this.deviceType != 'tablet'">
-            <span class="todayCard__value">{{ forecastWeatherData[0].day.avgvis_km + " " +
+        <BaseCard class="todayCard">
+            <span class="todayCard__value">{{ weatherData.vis_km + " " +
                 "km" }}</span>
             <p class="todayCard__text">Visible</p>
         </BaseCard>
     </BaseCard>
+
+    <TheAccordeon v-if="this.deviceType === 'tablet' || this.deviceType === 'mobile'" :title="'Weather details'">
+        <template v-slot:content>
+            <BaseCard class="todayCard">
+                <span class="todayCard__value"> {{ forecastWeatherData[0].day.daily_chance_of_rain + " " +
+                    "%" }}</span>
+                <p class="todayCard__text">Chance of rain</p>
+            </BaseCard>
+            <BaseCard class="todayCard">
+                <span class="todayCard__value">{{ astroData }}</span>
+                <p class="todayCard__text">{{ astroDataName }}</p>
+            </BaseCard>
+            <BaseCard class="todayCard">
+                <span class="todayCard__value"> {{ forecastWeatherData[0].day.maxtemp_c + " " +
+                    "C°" }}</span>
+                <p class="todayCard__text">Max temp</p>
+            </BaseCard>
+            <BaseCard class="todayCard">
+                <span class="todayCard__value">{{ forecastWeatherData[0].day.avghumidity + ' ' + '%' }}</span>
+                <p class="todayCard__text">Humidity</p>
+            </BaseCard>
+            <BaseCard class="todayCard">
+                <span class="todayCard__value">{{ forecastWeatherData[0].day.uv }}</span>
+                <p class="todayCard__text">UV</p>
+            </BaseCard>
+            <BaseCard class="todayCard">
+                <span class="todayCard__value">{{ weatherData.vis_km + " " +
+                    "km" }}</span>
+                <p class="todayCard__text">Visible</p>
+            </BaseCard>
+        </template>
+    </TheAccordeon>
 </template>
 <script>
 import { mapGetters } from 'vuex';
+import TheAccordeon from '@/UI/TheAccordeon.vue';
 export default {
+    components: { TheAccordeon },
     props: ['showTitle'],
     computed: {
-        ...mapGetters(['weatherData', 'forecastWeatherData']),
+        ...mapGetters(['weatherData', 'forecastWeatherData', 'deviceType']),
         astroData() {
             let result;
 
@@ -56,6 +90,9 @@ export default {
             return astroName
         }
     },
+    mounted() {
+        console.log(this.weatherData)
+    }
 }
 </script>
 <style scoped lang="sass">
